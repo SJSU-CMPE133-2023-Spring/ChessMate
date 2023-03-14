0269528c57fca36d16cfa6c08d8a46a2ccddd161
mik-fedosenko <mik-fedosenko@yandex.ru>
2023-03-14 14:16:38 -0700

    more move logic + displaying legal moves
    but it works a bit wrong

--- model.js
+++ model.js
@@ -3,29 +3,43 @@ let pieceSelected = false; //if true, then clicking on a highlighted square = mo
 
 
 // Model Variables
-var currentPosition = "R7/PP4PP/8/8/R5R1/8/8/RPR5";
+var currentPosition = "8/1R5R/R7/8/7R/8/8/5R2";
 var board = new Array(8);
 //setBoard(currentPosition);
-setBoard("R7/PP4PP/8/8/R5R1/8/8/RPR5");
+setBoard("8/1R5R/R7/8/7R/8/8/5R2");
 var gameID = 0;
 // Controller Methods
 function pieceClicked(element){
   if(!pieceSelected){
-    element.parentElement.style.background="#00FF00";
+    element.parentElement.style.background="#ffd500";
     //cut the piece from the potential id (ex. P_6 -> P);
     let piece = element.id.slice(0,1);
-    //cut the letter from the coordinates and transform into a number (ex. "b4" -> 2)
-    let x = parseInt(element.parentElement.id.slice(0,1), 36) - 9;
-    let y = element.parentElement.id.slice(1,2);
+    //cut the letter from the coordinates and transform into a number (ex. "b4" -> 1)
+    let x = parseInt(element.parentElement.id.slice(0,1), 36)-10;
+    let y = 8-element.parentElement.id.slice(1,2);
 
     console.log("clickedPiece(piece = "+ piece+", x = "+x+"; y = "+y+";");
-    getLegalMoves(piece, x, y);
-    addPiece(element, new Coordinate(6, 6));
-    console.log("piece added");
+    displayLegalMoves(getLegalMoves(piece, x, y));
+    console.log("pieceClicked is finished!");
+  } else {
+    hideLegalMoves();
+  }
+  pieceSelected = !pieceSelected;
+}
+
+function displayLegalMoves(legalMoves){
+  console.log("displaying n = "+legalMoves.length+" moves...");
+  for (let i = 0; i < legalMoves.length; i++) {
+    addLegalMove(new Coordinate(legalMoves[i].x,legalMoves[i].y));
   }
 }
 
-
+function hideLegalMoves(){
+  var i, elements = document.getElementsByClassName('legal-move-space-img');
+  for (i = elements.length; i--;) {
+    elements[i].parentNode.removeChild(elements[i]);
+  }
+}
 // Model methods
 function setBoard(newPosition){
   //newPosition = "R7/PP4PP/8/8/R5R1/8/8/RPR5";
@@ -51,38 +65,38 @@ function setBoard(newPosition){
     }
   }
   this.board = newBoard;
-  console.log("New board is set: ")
-  console.log(board);
+  logBoard();
 }
-// TODO: this method blocks the code, while it's unfinished
+
 function getLegalMoves(pieceType, x, y) {
-  console.log("here!");
   let legalMoves = [];
 
   switch (pieceType) {
     case "R": //for Rook
-        legalMoves.concat(getRookMoves(x, y));
+        legalMoves=(getRookMoves(x, y));
         break;
     case "P":
       console.log("Pawn");
 
   }
-  console.log("Total legal moves are: " + legalMoves);
+  console.log("GetLegalMoves: total number of moves =  " + legalMoves.length);
+  return legalMoves;
 }
 
 function getRookMoves(x, y){
-  return [].concat(checkNorth(x, y));
-  //return [].concat(checkNorth(x, y),checkEast(x, y), checkSouth(x, y), checkWest(x, y));
+  return [...checkNorth(x, y)];
+  //return [...checkNorth(x, y), ...checkEast(x, y), ...checkSouth(x, y), ...checkWest(x, y)];
 }
 
 function checkNorth(x, y){
   console.log("Checking North... x = " + x + "; y = " + y);
   let output = [];
-  let squaresTillEdge = y - 1; //or just y
+  let squaresTillEdge = y; //or just y
   while (squaresTillEdge > 0)// or > 0
   {
+    squaresTillEdge--;
     let nextChar = board[x][squaresTillEdge];
-    console.log("next char to check = " + nextChar);
+    console.log("next char to check = " + nextChar+" at x = "+x+"; y = "+ squaresTillEdge);
 
     //if empty spot - add
     if (nextChar === " ") {
@@ -97,22 +111,28 @@ function checkNorth(x, y){
     }
     //if friendly piece - break
     else if (nextChar >= 'A' && nextChar <= 'Z') {
-      console.log("ally detected! Stop the count!");
+      console.log("ally detected at x="+x+", y="+squaresTillEdge+"! Stop the count!");
       break;
     }
-      squaresTillEdge--;
   }
   console.log("North has " + output + " available positions. Size: " + output.length);
   return output;
 }
 
-function addPiece(piece, coordinates){
-  var img = document.createElement("img");
+function addLegalMove(coordinates){
+  let img = document.createElement("img");
   img.src = "green_circle.png";
-  img.className = "legal-move-space-img"
-  var src = piece.ownerDocument.getElementById(coordinates.getSquareName());
-  console.log("Coordinates of the piece added = " +"; The element = " + src)
+  img.className = "legal-move-space-img";
+  let src = document.getElementById(coordinates.getSquareName());
+  console.log("AddLegalMove: attempting to add a move at parentID = "+coordinates.getSquareName());
   src.appendChild(img);
+}
+
+function logBoard(){
+  console.log("Current board position is:")
+  for (let y = 0; y < 8; y++) {
+    console.log(board[y]);
+  }
 }
 class Coordinate {
   constructor(x, y) {
@@ -121,9 +141,9 @@ class Coordinate {
   }
 
   getSquareName(){
-    let letterX = String.fromCharCode(this.x+97);
-    console.log("squareName = "+ ""+letterX+this.y);
-    return ""+letterX+this.y;
+    //let letterX = String.fromCharCode(this.x+97);
+    console.log("GetSquareName: squareName = "+ ""+String.fromCharCode(this.x+97)+(8-this.y));
+    return ""+String.fromCharCode(this.x+97)+(8-this.y);
   }
 
 }
